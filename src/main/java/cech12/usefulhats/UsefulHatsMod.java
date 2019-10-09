007c75aa51d615da6863d1a8903af1f65bae8356
Christian Voß <cech12@gmail.com>
2019-10-09 12:35:44 +0200

    Forge MDK added - delete test mod - add initial files for new mod

---
+++ src/main/java/cech12/usefulhats/UsefulHatsMod.java
@@ -0,0 +1,20 @@
+package cech12.usefulhats;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static cech12.usefulhats.UsefulHatsMod.MOD_ID;
+
+@Mod(MOD_ID)
+public class UsefulHatsMod {
+
+    public static final String MOD_ID = "usefulhats";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public UsefulHatsMod() {
+        //TODO
+    }
+
+}
