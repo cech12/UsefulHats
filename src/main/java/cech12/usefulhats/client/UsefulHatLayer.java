bd85f3071f7cd4041e76ee666fb4127ffb7f3324
cech12 <cech12@gmail.com>
2019-10-26 16:38:37 +0200

    some changes in model rendering - use biped model functionalities (sneaking, swimming translation calculations) - use armor layer rendering (for smaller code)

--- src/main/java/cech12/usefulhats/client/UsefulHatLayer.java
+++ src/main/java/cech12/usefulhats/client/UsefulHatLayer.java
@@ -3,17 +3,17 @@ package cech12.usefulhats.client;
 import cech12.usefulhats.item.AbstractHatItem;
 import cech12.usefulhats.item.IUsefulHatModelOwner;
 import com.google.common.collect.Maps;
-import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
 import net.minecraft.client.renderer.entity.model.BipedModel;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.IDyeableArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 
 /**
@@ -26,22 +26,26 @@ public class UsefulHatLayer<T extends LivingEntity, M extends BipedModel<T>, A e
 
     private static final Map<String, ResourceLocation> ARMOR_TEXTURE_RES_MAP = Maps.newHashMap();
 
-    private final UsefulHatModel<T> hatModel = new UsefulHatModel<>(0.5F);
-
     public UsefulHatLayer(IEntityRenderer<T, M> renderer) {
-        super(renderer, null, null);
+        super(renderer, (A) new UsefulHatModel<T>(0.5F), (A) new UsefulHatModel<T>(0.5F));
     }
 
-    public ResourceLocation getArmorResource(ItemStack hatItemStack, String type) {
-        String texture = hatItemStack.getItem().getRegistryName().getPath();
-        String domain = hatItemStack.getItem().getRegistryName().getNamespace();
-        String s1 = String.format("%s:textures/models/usefulhats/%s%s.png", domain, texture, type == null ? "" : String.format("_%s", type));
-        ResourceLocation resourcelocation = ARMOR_TEXTURE_RES_MAP.get(s1);
-        if (resourcelocation == null) {
-            resourcelocation = new ResourceLocation(s1);
-            ARMOR_TEXTURE_RES_MAP.put(s1, resourcelocation);
+    @Override
+    public ResourceLocation getArmorResource(Entity entity, ItemStack stack, EquipmentSlotType slot, @Nullable String type) {
+        //texture location is another for this model (only for hats)
+        if (slot == EquipmentSlotType.HEAD) {
+            String texture = stack.getItem().getRegistryName().getPath();
+            String domain = stack.getItem().getRegistryName().getNamespace();
+            String s1 = String.format("%s:textures/models/usefulhats/%s%s.png", domain, texture, type == null ? "" : String.format("_%s", type));
+            ResourceLocation resourcelocation = ARMOR_TEXTURE_RES_MAP.get(s1);
+            if (resourcelocation == null) {
+                resourcelocation = new ResourceLocation(s1);
+                ARMOR_TEXTURE_RES_MAP.put(s1, resourcelocation);
+            }
+            return resourcelocation;
         }
-        return resourcelocation;
+        //to avoid errors in texture finding use super method
+        return super.getArmorResource(entity, stack, slot, type);
     }
 
     @Override
@@ -49,27 +53,18 @@ public class UsefulHatLayer<T extends LivingEntity, M extends BipedModel<T>, A e
         ItemStack hatItemStack = entityIn.getItemStackFromSlot(EquipmentSlotType.HEAD);
         Item hatItem = hatItemStack.getItem();
         if (hatItem instanceof AbstractHatItem && hatItem instanceof IUsefulHatModelOwner) {
-            GlStateManager.scalef(1.01F, 1.01F, 1.01F);
-            if (entityIn.isSneaking()) {
-                GlStateManager.translated(0, 0.250D, 0);
-            }
+            //super method makes its job good.
+            super.render(entityIn, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
+        }
+    }
 
-            this.bindTexture(this.getArmorResource(hatItemStack, null));
-            if (hatItem instanceof IDyeableArmorItem) {
-                int i = ((IDyeableArmorItem)hatItem).getColor(hatItemStack);
-                float f = (float)(i >> 16 & 255) / 255.0F;
-                float f1 = (float)(i >> 8 & 255) / 255.0F;
-                float f2 = (float)(i & 255) / 255.0F;
-                GlStateManager.color4f(f, f1, f2, 1.0F);
-                this.hatModel.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-                this.bindTexture(this.getArmorResource(hatItemStack, "overlay"));
-            }
-            GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-            this.hatModel.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-
-            if (hatItemStack.hasEffect()) {
-                func_215338_a(this::bindTexture, entityIn, this.hatModel, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, scale);
-            }
+    @Override
+    protected void setModelSlotVisible(A model, EquipmentSlotType slotIn) {
+        //disable all render models of biped model except the hat (because it is overridden with own model)
+        this.setModelVisible(model);
+        if (slotIn == EquipmentSlotType.HEAD && model instanceof UsefulHatModel) {
+            model.bipedHead.showModel = true;
+            model.bipedHeadwear.showModel = true;
         }
     }
 }
--- src/main/java/cech12/usefulhats/client/UsefulHatModel.java
+++ src/main/java/cech12/usefulhats/client/UsefulHatModel.java
@@ -1,55 +1,34 @@
 package cech12.usefulhats.client;
 
 import net.minecraft.client.renderer.entity.model.*;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 /**
  * Hat model class, which adds an hat flat addition (like straw hat for villagers) .
  * The hat is a half pixel higher than a normal helmet.
  */
-public class UsefulHatModel<T extends Entity> extends EntityModel<T> implements IHasHead, IHeadToggle {
+public class UsefulHatModel<T extends LivingEntity> extends BipedModel<T> {
 
-    protected final RendererModel innerHat;
-    protected final RendererModel outerHat;
-    protected final RendererModel hatAddition;
+    private final RendererModel hatAddition;
 
     public UsefulHatModel(float scale) {
         this(scale, 64, 48);
     }
 
     public UsefulHatModel(float scale, int p_i51059_2_, int p_i51059_3_) {
-        this.innerHat = (new RendererModel(this)).setTextureSize(p_i51059_2_, p_i51059_3_);
-        this.innerHat.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.innerHat.setTextureOffset(0, 0).addBox(-4.0F, -8.5F, -4.0F, 8, 8, 8, scale);
-        this.outerHat = (new RendererModel(this)).setTextureSize(p_i51059_2_, p_i51059_3_);
-        this.outerHat.setRotationPoint(0.0F, 0.0F, 0.0F);
-        this.outerHat.setTextureOffset(32, 0).addBox(-4.0F, -8.5F, -4.0F, 8, 8, 8, scale + 0.5F);
-        this.innerHat.addChild(this.outerHat);
+        //override render hat models of BipedModel
+        this.bipedHead = (new RendererModel(this)).setTextureSize(p_i51059_2_, p_i51059_3_);
+        this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.bipedHead.setTextureOffset(0, 0).addBox(-4.0F, -8.5F, -4.0F, 8, 8, 8, scale);
+        this.bipedHeadwear = (new RendererModel(this)).setTextureSize(p_i51059_2_, p_i51059_3_);
+        this.bipedHeadwear.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.bipedHeadwear.setTextureOffset(32, 0).addBox(-4.0F, -8.5F, -4.0F, 8, 8, 8, scale + 0.5F);
         this.hatAddition = (new RendererModel(this)).setTextureSize(p_i51059_2_, p_i51059_3_);
         this.hatAddition.setRotationPoint(0.0F, 0.0F, 0.0F);
         this.hatAddition.setTextureOffset(0, 31).addBox(-8.0F, -8.0F, -5.5F - scale*2, 16, 16, 1, scale*2);
         this.hatAddition.rotateAngleX = (-(float)Math.PI / 2F);
-        this.outerHat.addChild(this.hatAddition);
+        //add hat addition to head wear as child. So no extra calculations must be done.
+        this.bipedHeadwear.addChild(this.hatAddition);
     }
 
-    public void render(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        this.setRotationAngles(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-        this.innerHat.render(scale);
-    }
-
-    public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor) {
-        this.innerHat.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
-        this.innerHat.rotateAngleX = headPitch * ((float)Math.PI / 180F);
-        this.innerHat.rotateAngleZ = 0.0F;
-    }
-
-    public RendererModel func_205072_a() {
-        return this.innerHat;
-    }
-
-    public void func_217146_a(boolean p_217146_1_) {
-        this.innerHat.showModel = p_217146_1_;
-        this.outerHat.showModel = p_217146_1_;
-        this.hatAddition.showModel = p_217146_1_;
-    }
 }
