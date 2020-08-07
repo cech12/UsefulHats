3fc6a5c9a4e18e2326c20804eb8f8f35085f216f
cech12 <cech12@gmail.com>
2020-08-07 12:26:17 +0200

    - Zombified Piglins are agressive against players wearing a halo in Nether

--- src/main/java/cech12/usefulhats/item/HaloItem.java
+++ src/main/java/cech12/usefulhats/item/HaloItem.java
@@ -4,6 +4,7 @@ import cech12.usefulhats.config.Config;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.monster.ZombifiedPiglinEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
@@ -12,7 +13,6 @@ import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
@@ -29,22 +29,19 @@ public class HaloItem extends AbstractHatItem implements IAttackTargetChanger, I
     public void onItemToolTipEvent(ItemStack stack, List<ITextComponent> tooltip) {
         super.onItemToolTipEvent(stack, tooltip);
         //tooltip.add(new TranslationTextComponent("item.usefulhats.halo.desc.no_attack").applyTextStyle(TextFormatting.BLUE));
-        tooltip.add(new TranslationTextComponent("item.usefulhats.halo.desc.beware_of_nether").func_240699_a_(TextFormatting.BLUE));
+        tooltip.add(new TranslationTextComponent("item.usefulhats.halo.desc.no_attack").func_240699_a_(TextFormatting.BLUE));
         //tooltip.add(new TranslationTextComponent("item.usefulhats.halo.desc.beware_of_nether").applyTextStyle(TextFormatting.RED));
         tooltip.add(new TranslationTextComponent("item.usefulhats.halo.desc.beware_of_nether").func_240699_a_(TextFormatting.RED));
     }
 
-    private static boolean isPlayerInNether(PlayerEntity player) {
-        //TODO get players dimension
-        //return player.dimension == DimensionType.THE_NETHER;
-        //return player.dimension == DimensionType.field_236000_d_;
-        return false;
+    private static boolean isEntityInNether(Entity entity) {
+        return entity.world.func_230315_m_().func_241511_k_();
     }
 
     @Override
     public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
         // Looses durability outside the nether when non-boss mobs are in range (X blocks)
-        if (!isPlayerInNether(player)) {
+        if (!isEntityInNether(player)) {
             Vector3d playerPos = player.getPositionVec();
             int range = Config.HALO_DETECTING_RANGE.getValue();
             AxisAlignedBB radius = new AxisAlignedBB(playerPos.getX()-range, playerPos.getY()-range, playerPos.getZ()-range, playerPos.getX()+range, playerPos.getY()+range, playerPos.getZ()+range);
@@ -57,17 +54,22 @@ public class HaloItem extends AbstractHatItem implements IAttackTargetChanger, I
 
     @Override
     public void onEntityJoinWorldEvent(MobEntity entity, EntityJoinWorldEvent event) {
-        // add attack goal to ZombiePigmanEntity against players with halo on (only in nether)
-        //TODO 1.16 entities
-        //if (entity instanceof ZombiePigmanEntity) {
-        //    entity.targetSelector.addGoal(1, new NearestHaloTargetGoal(entity, this));
-        //}
+        // add attack goal to all neutral nether mobs against players with halo on (only in nether)
+        // 1.15: (outdated)
+        // - Zombie Pigman
+        // 1.16:
+        // - Hoglins/Zoglins (hostile) - nothing to do
+        // - Piglins (hostile except golden armor) - TODO add the goal unrelated to golden armor - difficult because of its brain implementation
+        // - Zombified Piglins (neutral) - add the goal
+        if (entity instanceof ZombifiedPiglinEntity) {
+            entity.targetSelector.addGoal(1, new NearestHaloTargetGoal(entity, this));
+        }
     }
 
     @Override
     public void onLivingSetAttackTarget(MobEntity entity, PlayerEntity targetPlayer) {
         // avoid to get attacked from non-boss mob entities outside the nether
-        if (entity.isNonBoss() && !isPlayerInNether(targetPlayer)) {
+        if (entity.isNonBoss() && !isEntityInNether(targetPlayer)) {
             entity.setAttackTarget(null);
         }
     }
@@ -77,7 +79,7 @@ public class HaloItem extends AbstractHatItem implements IAttackTargetChanger, I
             super(mobEntity, PlayerEntity.class, 0, true, false,
                     (entity) -> (entity instanceof PlayerEntity &&
                             entity.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() == hatItem &&
-                            isPlayerInNether((PlayerEntity) entity)));
+                            isEntityInNether(entity)));
         }
     }
 }
