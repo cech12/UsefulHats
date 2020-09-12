2cdfa948ad8666c24e333ddc34a710b2b4e76f41
cech12 <cech12@gmail.com>
2020-09-12 19:20:36 +0200

    cleanup doubled implemented methods

--- src/main/java/cech12/usefulhats/item/ChoppingHatItem.java
+++ src/main/java/cech12/usefulhats/item/ChoppingHatItem.java
@@ -1,23 +1,24 @@
 package cech12.usefulhats.item;
 
 import cech12.usefulhats.config.Config;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.ToolType;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.BlockEvent;
 
 import java.util.List;
+import java.util.Set;
 
-public class ChoppingHatItem extends AbstractMiningHatItem implements IBreakSpeedChanger {
+public class ChoppingHatItem extends AbstractMiningHatItem {
 
     public ChoppingHatItem() {
         super("chopping_hat", HatArmorMaterial.CHOPPING, rawColorFromRGB(91, 91, 91), Config.CHOPPING_HAT_ENABLED, Config.CHOPPING_HAT_DAMAGE_ENABLED);
     }
 
-    private double[] getSpeedConfig() {
+    @Override
+    protected double[] getSpeedConfig() {
         double[] speedConfig = new double[6];
         speedConfig[0] = Config.CHOPPING_HAT_SPEED_WITH_EFFICIENCY_0.getValue();
         speedConfig[1] = Config.CHOPPING_HAT_SPEED_WITH_EFFICIENCY_1.getValue();
@@ -36,17 +37,7 @@ public class ChoppingHatItem extends AbstractMiningHatItem implements IBreakSpee
     }
 
     @Override
-    public void onBreakSpeedEvent(PlayerEvent.BreakSpeed event, ItemStack headSlotItemStack) {
-        if (!event.isCanceled() && event.getPlayer().getHeldItemMainhand().getToolTypes().contains(ToolType.AXE) && event.getState().isToolEffective(ToolType.AXE)) {
-            //use getNewSpeed() instead of getOriginalSpeed() to support other mods that are changing the break speed with this event.
-            event.setNewSpeed((1.0F + (float) this.getEnchantmentValue(headSlotItemStack, this.getSpeedConfig())) * event.getNewSpeed());
-        }
-    }
-
-    @Override
-    public void onBreakEvent(BlockEvent.BreakEvent event, ItemStack headSlotItemStack) {
-        if (!event.isCanceled() && event.getPlayer().getHeldItemMainhand().getToolTypes().contains(ToolType.AXE) && event.getState().isToolEffective(ToolType.AXE)) {
-            this.damageHatItemByOne(headSlotItemStack, event.getPlayer());
-        }
+    protected boolean isToolEffective(Set<ToolType> toolTypes, BlockState state) {
+        return toolTypes.contains(ToolType.AXE) && state.isToolEffective(ToolType.AXE);
     }
 }
