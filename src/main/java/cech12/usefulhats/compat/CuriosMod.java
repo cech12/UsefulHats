86f6505557e26530b3bba0d3364e5c1ce9456d87
cech12 <cech12@gmail.com>
2020-09-22 09:42:17 +0200

    add a config for adding a curios head slot (default false) #32

--- src/main/java/cech12/usefulhats/compat/CuriosMod.java
+++ src/main/java/cech12/usefulhats/compat/CuriosMod.java
@@ -1,13 +1,25 @@
 package cech12.usefulhats.compat;
 
+import cech12.usefulhats.config.Config;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 public class CuriosMod {
+
+    public static final String CURIOS_ID = "curios";
 
     private CuriosMod() {}
 
     public static boolean isLoaded() {
-        return ModList.get().isLoaded("curios");
+        return ModList.get().isLoaded(CURIOS_ID);
+    }
+
+    public static void addHeadSlot() {
+        if (isLoaded() && Config.CURIOS_ADD_HEAD_SLOT.getValue()) {
+            InterModComms.sendTo(CURIOS_ID, CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("head"));
+        }
     }
 
 }
