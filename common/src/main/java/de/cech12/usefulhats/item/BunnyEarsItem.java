f059e3e24064ca8b514da9b69669f153253fa0dc
cech12 <cech12@gmail.com>
2025-10-13 11:27:33 +0200

    added config options for disabling Eat Boost or Jump Boost of Bunny Ears #77

--- common/src/main/java/de/cech12/usefulhats/item/BunnyEarsItem.java
+++ common/src/main/java/de/cech12/usefulhats/item/BunnyEarsItem.java
@@ -30,13 +30,17 @@ public class BunnyEarsItem extends AbstractHatItem implements IEquipmentChangeLi
     public void appendHoverText(@NotNull ItemStack stack, @NotNull Item.TooltipContext context, @NotNull List<Component> tooltip, @NotNull TooltipFlag flagIn) {
         super.appendHoverText(stack, context, tooltip, flagIn);
         int enchantmentLevel = CommonLoader.getEnchantmentLevel(stack, Enchantments.EFFICIENCY) + 1;
-        tooltip.add(Component.translatable("item.usefulhats.bunny_ears.desc.jump_boost", UsefulHatsUtils.getRomanNumber(enchantmentLevel, false)).withStyle(ChatFormatting.BLUE));
-        tooltip.add(Component.translatable("item.usefulhats.bunny_ears.desc.eating", enchantmentLevel + 1).withStyle(ChatFormatting.BLUE));
+        if (Services.CONFIG.isBunnyEarsJumpBoostEnabled()) {
+            tooltip.add(Component.translatable("item.usefulhats.bunny_ears.desc.jump_boost", UsefulHatsUtils.getRomanNumber(enchantmentLevel, false)).withStyle(ChatFormatting.BLUE));
+        }
+        if (Services.CONFIG.isBunnyEarsEatBoostEnabled()) {
+            tooltip.add(Component.translatable("item.usefulhats.bunny_ears.desc.eating", enchantmentLevel + 1).withStyle(ChatFormatting.BLUE));
+        }
     }
 
     @Override
-    public void inventoryTick(@NotNull ItemStack stack, Level level, @NotNull Entity entity, int slot, boolean selectedIndex) {
-        if (!level.isClientSide && entity instanceof LivingEntity livingEntity) {
+    public void inventoryTick(@NotNull ItemStack stack, @NotNull Level level, @NotNull Entity entity, int slot, boolean selectedIndex) {
+        if (Services.CONFIG.isBunnyEarsJumpBoostEnabled() && !level.isClientSide && entity instanceof LivingEntity livingEntity) {
             if (!Services.REGISTRY.getEquippedHatItemStacks(livingEntity).contains(stack)) return; //only one worn stack of this item should add its effect
             int amplifier = CommonLoader.getEnchantmentLevel(stack, Enchantments.EFFICIENCY);
             if (!this.isEffectCausedByOtherSource(livingEntity, MobEffects.JUMP, JUMP_BOOST_DURATION, amplifier)) {
@@ -49,7 +53,7 @@ public class BunnyEarsItem extends AbstractHatItem implements IEquipmentChangeLi
 
     @Override
     public int onItemUseEventStart(LivingEntity entity, ItemStack usedStack, int actualDuration, ItemStack headSlotItemStack) {
-        if (usedStack.has(DataComponents.FOOD)) {
+        if (Services.CONFIG.isBunnyEarsEatBoostEnabled() && usedStack.has(DataComponents.FOOD)) {
             int amplifier = CommonLoader.getEnchantmentLevel(headSlotItemStack, Enchantments.EFFICIENCY) + 2;
             this.damageHatItemByOne(headSlotItemStack, entity);
             return actualDuration / amplifier;
@@ -59,16 +63,20 @@ public class BunnyEarsItem extends AbstractHatItem implements IEquipmentChangeLi
 
     @Override
     public void onUnequippedHatItem(LivingEntity entity, ItemStack oldStack) {
-        // disable effects when hat is removed from slot
-        int amplifier = CommonLoader.getEnchantmentLevel(oldStack, Enchantments.EFFICIENCY);
-        this.removeEffect(entity, MobEffects.JUMP, JUMP_BOOST_DURATION, amplifier);
+        if (Services.CONFIG.isBunnyEarsJumpBoostEnabled()) {
+            // disable effects when hat is removed from slot
+            int amplifier = CommonLoader.getEnchantmentLevel(oldStack, Enchantments.EFFICIENCY);
+            this.removeEffect(entity, MobEffects.JUMP, JUMP_BOOST_DURATION, amplifier);
+        }
     }
 
     @Override
     public void onLivingJumpEvent(LivingEntity jumpingEntity, ItemStack headSlotItemStack) {
-        int amplifier = CommonLoader.getEnchantmentLevel(headSlotItemStack, Enchantments.EFFICIENCY);
-        if (!this.isEffectCausedByOtherSource(jumpingEntity, MobEffects.JUMP, JUMP_BOOST_DURATION, amplifier)) {
-            this.damageHatItemByOne(headSlotItemStack, jumpingEntity);
+        if (Services.CONFIG.isBunnyEarsJumpBoostEnabled()) {
+            int amplifier = CommonLoader.getEnchantmentLevel(headSlotItemStack, Enchantments.EFFICIENCY);
+            if (!this.isEffectCausedByOtherSource(jumpingEntity, MobEffects.JUMP, JUMP_BOOST_DURATION, amplifier)) {
+                this.damageHatItemByOne(headSlotItemStack, jumpingEntity);
+            }
         }
     }
 }
