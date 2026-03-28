d784707927bf47c2f0dde14f5978886fc92eeea4
cech12 <cech12@gmail.com>
2026-03-28 12:07:12 +0100

    re-added Curios support (NeoForge)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [this versioning scheme](https://gist.github.com/cech12/69319028e88c50349a6b044000a6607b).
 
+## [1.21.8-6.10.1.0] - 2026-03-28
+### Added
+- re-added Curios support (NeoForge)
+
 ## [1.21.8-6.10.0.0] - 2025-10-13
 ### Changed
 - updated to Minecraft 1.21.8 (Fabric 0.134.0+1.21.8, NeoForge 21.8.47, Forge 58.1.0)
--- neoforge/src/main/java/de/cech12/usefulhats/platform/NeoForgeRegistryHelper.java
+++ neoforge/src/main/java/de/cech12/usefulhats/platform/NeoForgeRegistryHelper.java
@@ -2,6 +2,7 @@ package de.cech12.usefulhats.platform;
 
 import de.cech12.usefulhats.Constants;
 import de.cech12.usefulhats.compat.AccessoriesCompat;
+import de.cech12.usefulhats.compat.CuriosCompat;
 import de.cech12.usefulhats.init.ModItems;
 import de.cech12.usefulhats.item.AbstractHatItem;
 import de.cech12.usefulhats.platform.services.IRegistryHelper;
@@ -44,6 +45,9 @@ public class NeoForgeRegistryHelper implements IRegistryHelper {
         }
         if (Services.PLATFORM.isModLoaded(Constants.ACCESSORIES_MOD_ID)) {
             AccessoriesCompat.addEquippedHatsToList(entity, stacks);
+        }
+        if (Services.PLATFORM.isModLoaded(Constants.CURIOS_MOD_ID)) {
+            CuriosCompat.addEquippedHatsToList(entity, stacks);
         }
         return stacks;
     }
