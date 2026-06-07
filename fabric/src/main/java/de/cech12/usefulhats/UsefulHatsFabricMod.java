7fb6ac16184ba20cddd21397c99968a2b4d0a73f
cech12 <cech12@gmail.com>
2026-06-07 08:40:00 +0200

    re-added Trinktes (Canary) support (Fabric/Quilt)

--- fabric/src/main/java/de/cech12/usefulhats/UsefulHatsFabricMod.java
+++ fabric/src/main/java/de/cech12/usefulhats/UsefulHatsFabricMod.java
@@ -1,6 +1,7 @@
 package de.cech12.usefulhats;
 
 import de.cech12.usefulhats.compat.AccessoriesCompat;
+import de.cech12.usefulhats.compat.TrinketsCompat;
 import de.cech12.usefulhats.init.ModCreativeTabs;
 import de.cech12.usefulhats.init.ModItems;
 import de.cech12.usefulhats.platform.Services;
@@ -14,9 +15,9 @@ public class UsefulHatsFabricMod implements ModInitializer {
         ModItems.init();
         ModCreativeTabs.init();
 
-        //if (Services.PLATFORM.isModLoaded(TrinketsCompat.MOD_ID)) {
-        //    TrinketsCompat.register();
-        //}
+        if (Services.PLATFORM.isModLoaded(TrinketsCompat.MOD_ID)) {
+            TrinketsCompat.register();
+        }
         if (Services.PLATFORM.isModLoaded(Constants.ACCESSORIES_MOD_ID)) {
             AccessoriesCompat.register();
         }
--- fabric/src/main/java/de/cech12/usefulhats/client/compat/TrinketsClientCompat.java
+++ fabric/src/main/java/de/cech12/usefulhats/client/compat/TrinketsClientCompat.java
@@ -1,5 +1,5 @@
 package de.cech12.usefulhats.client.compat;
-/*
+
 import com.mojang.blaze3d.vertex.PoseStack;
 import de.cech12.usefulhats.client.AbstractUsefulHatsRenderer;
 import de.cech12.usefulhats.platform.Services;
@@ -7,9 +7,9 @@ import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.client.TrinketRenderer;
 import dev.emi.trinkets.api.client.TrinketRendererRegistry;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -27,15 +27,9 @@ public class TrinketsClientCompat {
     private static class UsefulHatsTrinketRenderer extends AbstractUsefulHatsRenderer implements TrinketRenderer {
 
         @Override
-        public void render(ItemStack stack, SlotReference slotReference, EntityModel<? extends LivingEntity> contextModel, PoseStack matrices, MultiBufferSource vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
-            this.render(stack, matrices, vertexConsumers, light, entity, limbAngle, limbDistance, tickDelta, animationProgress, headYaw, headPitch);
-        }
-
-        @Override
-        protected void followBodyRotations(LivingEntity livingEntity, HumanoidModel<LivingEntity> humanoidModel) {
-            TrinketRenderer.followBodyRotations(livingEntity, humanoidModel);
+        public void render(ItemStack itemStack, SlotReference slotReference, EntityModel<? extends LivingEntityRenderState> entityModel, PoseStack poseStack, MultiBufferSource multiBufferSource, int light, LivingEntityRenderState livingEntityRenderState, float v, float v1) {
+            this.render(itemStack, poseStack, multiBufferSource, light, (HumanoidRenderState)livingEntityRenderState);
         }
     }
 
 }
- */
